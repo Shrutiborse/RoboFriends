586b36dd782d935dddc417c1a7bd16ff37a69683
Shruti Borse <shrutiborse11@gmail.com>
2022-07-19 14:55:25 +0530

    Adding Starting Project

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React,{ Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state={
+            Robots: [],
+            searchfield:''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json()
+        .then(user=> this.setState({Robots: user})));
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render(){
+        const {Robots,searchfield} = this.state;
+        const filteredRobots=Robots.filter(Robot =>{
+            return Robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !Robots.length?
+             <h1>Loading...</h1>:
+             (
+                <div className='tc'>
+                    <h1 className='f1'>Robo Friends</h1>
+                    <SearchBox  searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList Robots={filteredRobots} />
+                    </Scroll>
+                </div>
+             );
+        }
+    }
+export default App;
